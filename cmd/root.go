09704d37119c606aedcd9e00caa044f5dd3377fc
Stefan Kroschel <skroschel@slashwhy.de>
2023-04-13 12:18:04 +0200

    Update command description

--- cmd/root.go
+++ cmd/root.go
@@ -27,18 +27,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "yarnique",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Prettifies yarn list JSON output into an SVG image",
+	Long: `yarnique is a tool to visualize yarn list output
+by taking a yarn list JSON output file and converting it to an SVG image`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
@@ -64,5 +58,3 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
-
